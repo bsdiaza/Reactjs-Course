ce9c65f73d170cf58b438f8c183f50bce2aebfe5
bdiazabuyc <bdiazabuyc@gmail.com>
2019-06-07 17:13:30 -0500

    Section 6

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import { SUN } from '../../constants/weathers';
 import './styles.css';
 
-const WeatherLocation = () => ( 
-  <div className="weatherLocationCont">
-    <Location city={"Bogota"}/>
-    <WeatherData/>
-  </div>
-)
+const data = {
+  temperature: 5,
+  weatherState: SUN,
+  humidity:10,
+  wind: '10m/s'
+}
+
+class WeatherLocation extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      city: "Bogota",
+      data: data
+    }
+  }
+
+  handleUpdateClick = () =>{
+    this.setState({
+      city: "lalaland"
+    });
+  }
+
+  render(){
+    const { city, data } = this.state;
+    return ( 
+      <div className="weatherLocationCont">
+        <Location city={city}/>
+        <WeatherData data={data}/>  
+        <button onClick={this.handleUpdateClick}>Actualizar</button>
+      </div>
+    )
+  }
+}
 
 export default WeatherLocation;
