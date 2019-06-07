98a0c0b286c1a4187737579d16b8aaf903db0549
bdiazabuyc <bdiazabuyc@gmail.com>
2019-06-07 10:26:05 -0500

    Section 3 - 12

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import WeatherLocation from './WheatherLocation';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <WeatherLocation></WeatherLocation>
+      </header>
+    </div>
+  );
+}
+
+export default App;
