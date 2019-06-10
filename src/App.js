2b3c649d89a51ca5d8c9402c90b55618aedfd367
Disarrollo3 <disarrollo3@localhost.localdomain>
2019-06-10 16:01:50 -0500

    Section 10

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { Grid, Col, Row } from 'react-flexbox-grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 import './App.css';
 import LocationList from './components/LocationList';
 
@@ -17,12 +22,30 @@ class App extends Component{
   }
   render(){
     return (
-      <div className="App">
-        <LocationList 
-          cities={cities} 
-          onSelectedLocation={this.handleOnSelectionLocation}
-        />
-      </div>
+      <Grid>
+        <Row>
+          <AppBar position='sticky'>
+            <Toolbar>
+              <Typography variant="h5" color="inherit">
+                Weather App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}> 
+            <LocationList 
+              cities={cities} 
+              onSelectedLocation={this.handleOnSelectionLocation}
+            />
+          </Col>
+          <Col xs={12} md={6}>
+            <Paper elevation={4}>
+              <div className="details"></div>
+            </Paper>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
